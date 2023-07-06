79b3006339ad97853824516007571ee2a8aa8fbd
Arna Ghosh <arnatubaikgp@gmail.com>
2023-07-06 13:11:32 -0400

    Changing np.float to float because of updated numpy version

--- utils/network.py
+++ utils/network.py
@@ -45,7 +45,8 @@ def get_base_network(net_hparams: dict):
     return base_network
 
 def calc_coeff(iter_num, high=1.0, low=0.0, alpha=10.0, max_iter=10000.0):
-    return np.float(2.0 * (high - low) / (1.0 + np.exp(-alpha*iter_num / max_iter)) - (high - low) + low)
+    # return np.float(2.0 * (high - low) / (1.0 + np.exp(-alpha*iter_num / max_iter)) - (high - low) + low)
+    return float(2.0 * (high - low) / (1.0 + np.exp(-alpha*iter_num / max_iter)) - (high - low) + low)
 
 def init_weights(m):
     classname = m.__class__.__name__
