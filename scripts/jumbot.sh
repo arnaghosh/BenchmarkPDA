58362a5e305dc9c69cb448afa6b94593c8c9f938
Arna Ghosh <arnatubaikgp@gmail.com>
2023-07-07 18:29:16 -0400

    Adding sbatch script for jumbot

---
+++ scripts/jumbot.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#SBATCH --array=0-134%60
+#SBATCH --partition=long
+#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --mem=10GB
+#SBATCH --time=2:00:00
+#SBATCH --cpus-per-gpu=4
+#SBATCH --output=sbatch_out/jumbot.%A.%a.out
+#SBATCH --error=sbatch_err/jumbot.%A.%a.err
+#SBATCH --job-name=jumbot
+
+. /etc/profile
+module load anaconda/3
+conda activate ffcv
+
+python hp_search_train_val.py --method jumbot --dset office-home \
+                            --source_domain Art --target_domain Clipart \
+                            --sweep_idx $SLURM_ARRAY_TASK_ID
